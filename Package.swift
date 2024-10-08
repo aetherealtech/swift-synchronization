44ed32b87c632f4417528b066d0472ec457ca4f2
Dan Coleman <dan@aetherealtech.com>
2024-10-08 06:28:41 -0500

    Update packages

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/aetherealtech/swift-assertions", branch: "master"),
+        .package(url: "https://github.com/aetherealtech/swift-assertions", from: .init(0, 1, 0)),
     ],
     targets: [
         .target(
